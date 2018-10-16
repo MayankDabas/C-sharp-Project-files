dc1ec74f39fea6dd5ac0bfe48a66241653890266
Mayank Dabas <mayankdabas2401@gmail.com>
2018-10-16 20:48:15 +0530

    Use of for iteration

---
+++ ForIteration/ForIteration/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* for (int i = 0; i < 10; i++)
+             {
+                 //Console.WriteLine(i);
+
+                 if (i == 8)
+                 {
+                     Console.WriteLine("Found eight!");
+                     break;
+                 }
+             }*/
+
+            for (int myValue = 0; myValue < 10; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+            Console.ReadLine();
+        }
+    }
+}
