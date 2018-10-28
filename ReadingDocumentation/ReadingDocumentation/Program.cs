cf65f46d2865a4ca6815cabd661d7574b2331d93
Mayank Dabas <mayankdabas2401@gmail.com>
2018-10-28 12:07:44 +0530

    Reading Documentation

---
+++ ReadingDocumentation/ReadingDocumentation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingDocumentation
+{
+    /// <summary>
+    /// Reading Documentation
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demostrates reading documentation
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            float cosine = (float)Math.Cos(45 * Math.PI / 180);
+
+            //Calculate and print cosine of 45 degrees
+
+            Console.WriteLine("Cosine of 45 degree is: " + cosine);
+            Console.ReadLine();
+        }
+    }
+}
